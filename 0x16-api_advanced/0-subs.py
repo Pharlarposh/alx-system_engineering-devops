f6e8e9d45f620449511657d0b987fc88d0f4b98d
Omobolaji Olusanya <aderomolabolaji@gmail.com>
2024-04-10 01:00:12 +0100

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit."""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
 
-def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}  
-    response = requests.get(url, headers=headers)
-
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        if 'data' in data and 'children' in data['data']:
-            for post in data['data']['children']:
-                print(post['data']['title'])
-        else:
-            print("No posts found.")
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        print("None")
+        return 0
