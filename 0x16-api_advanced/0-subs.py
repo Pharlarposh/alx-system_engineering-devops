cc0c423593338aa363899e7b531950f9e552a3cc
Pharlarposh <aderomolabolaji@gmail.com>
2024-06-06 19:49:20 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -21,10 +21,13 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'myAPI/0.0.1'}
     
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data['data'].get('subscribers', 0)
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json()
+            return data['data'].get('subscribers', 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
